0658f329bce81b1c611015dbc7c354eee15382f5
PC-Yassine <PC-Yassine@PC-YASSINE.local>
2021-02-28 03:28:39 +0100

    Update MarketPlaceService.java to implements the interface

--- src/mercato/DAO/Classe/MarketPlaceService.java
+++ src/mercato/DAO/Classe/MarketPlaceService.java
@@ -5,10 +5,40 @@
  */
 package mercato.DAO.Classe;
 
+import java.util.List;
+import mercato.DAO.Interface.MarketPlaceInterface;
+import mercato.Model.Product;
+import mercato.Model.SponsoredUser;
+
 /**
  *
  * @author PC-Yassine
  */
-public class MarketPlaceService {
+public class MarketPlaceService implements MarketPlaceInterface{
+
+    @Override
+    public boolean addProduct(Product p) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean UpdateProduct(Product p) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Product> ListAll() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Product getById(int id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Product getBySponsoredUser(SponsoredUser sp) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
